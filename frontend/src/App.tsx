e779034a855653be1386dcd9bf8481df556c414c
kwy <kwy1762@naver.com>
2025-07-25 21:44:28 +0900

    tailwind css, shadcn, react-router-dom 초기 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 // import { useEffect, useState } from 'react'
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
-import './App.css';
+// import './App.css';
 // import axios from 'axios'
+
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   // const [message, setMessage] = useState([]);
@@ -14,9 +17,9 @@ function App() {
   // }, []);
 
   return (
-    <>
-      <h1>environment</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center">홈 페이지</h1>
+      <h2 className="text-center">shadcn components</h2>
+    </div>
+  );
+}
