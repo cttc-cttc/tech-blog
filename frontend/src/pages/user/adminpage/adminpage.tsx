bda1a0f558dfd1a75a88449f30ec2e7123832b82
kwy <kwy1762@naver.com>
2025-08-25 01:24:53 +0900

    admin dashboard 레이아웃 수정

--- frontend/src/pages/user/adminpage/adminpage.tsx
+++ frontend/src/pages/user/adminpage/adminpage.tsx
@@ -1,7 +1,7 @@
 export default function Adminpage() {
   return (
-    <div className="max-w-4xl flex flex-col gap-5">
-      <div className="bg-muted/50 w-full min-h-[40vh] rounded-xl"></div>
+    <div className="w-full max-w-4xl flex flex-col gap-5 self-baseline ml-8">
+      <div className="bg-muted/50 w-full min-h-[50vh] rounded-xl"></div>
       <div className="flex gap-5">
         <div className="flex-1 bg-muted/50 min-h-[60vh] rounded-xl"></div>
         <div className="flex-1 bg-muted/50 min-h-[60vh] rounded-xl"></div>
